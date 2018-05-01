a08a79f8ff91ac4091a17e8d79cca9d580d4b208
karthigeyankalyan <karthigeyankalyan@gmail.com>
2018-05-02 00:12:59 +0530

    Checking python filtering

--- src/app.py
+++ src/app.py
@@ -99,10 +99,11 @@ def all_employees():
         employee = Employee.from_mongo_blog()
         return render_template('all_beneficiaries.html', employee=employee)
 
-@app.route('/block_beneficiaries/<string:Block>')
+@app.route('/block_beneficiaries/<string:District>/<string:Block>')
 @app.route('/block_beneficiaries')
-def block_employees(Block):
-    return render_template('block_beneficiaries.html', block=Block)
+def block_employees(District, Block):
+    emp = Database.find("employees", {'District': District, 'Block': Block})
+    return render_template('abc.html', block=Block, emp=emp)
 
 @app.route('/retirement_employees')
 def retired_employees():
