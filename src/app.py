e5bab7a75d3596fa79fb38a9c158b0fc13697265
karthigeyankalyan <karthigeyankalyan@gmail.com>
2019-05-28 11:57:15 +0530

    Checking

--- src/app.py
+++ src/app.py
@@ -73,7 +73,7 @@ def enter_dates_to_get_retirements_within(District, Block):
         end_date = request.form['endDate']
 
         if user.designation == "Block Operator":
-            return render_template('all_retirements_district.html', user=user, start_date=start_date,
+            return render_template('all_retirements_district_block.html', user=user, start_date=start_date,
                                    end_date=end_date, district=District, block=Block)
         elif user.designation == "Admin":
             return render_template('all_retirements_admin.html', user=user, start_date=start_date,
